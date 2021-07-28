1d4370b63230b8dca1be42edf704cd32401d8660
dynamicdreamz <info@dynamicdreamz.com>
2021-07-28 11:07:05 +0530

    done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,10 +43,7 @@
           </a>
         </div>
       </header>
-
-
-
-
+      
       <div class="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
         <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
             <div class="mb-4">
